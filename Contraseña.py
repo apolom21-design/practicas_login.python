b33d4ff0a059e49751dd2a3f2064eebed1bfdbf1
apolom21-design <apolom21@campusuninunez.edu.co>
2025-11-09 17:34:20 -0500

    first commit

---
+++ Contraseña.py
@@ -0,0 +1,8 @@
+import string
+import random
+
+longitud = int(input("Ingrese la longitud de la contraseña: "))
+caracteres = string.ascii_letters + string.digits + string.punctuation
+contrasena = ''.join(random.choice(caracteres) for i in range(longitud))
+
+print("contraseña generada: "+ contrasena)
